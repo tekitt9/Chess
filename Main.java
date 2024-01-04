9bdfe2b96c144adbda4083f46b9914f417c71016
Lev <levlekhminka@gmail.com>
2024-01-04 18:46:50 +0100

    Fixed empty Main class

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Game game = new Game(new Scanner(System.in));
+        game.start();
     }
 }
